84b0745fe39362e9927b5d2d30323553628c325b
Albertyhu <hualbert@rocketmail.com>
2023-07-12 19:58:38 -0700

    fixed issues with preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,58 @@
 import { defineConfig } from 'astro/config';
-
+import NetlifyCMS from 'astro-netlify-cms';
 import tailwind from "@astrojs/tailwind";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), NetlifyCMS({
+    config: {
+      backend: {
+        name: 'git-gateway',
+        branch: 'main'
+      },
+      collections: [{
+        name: "posts",
+        label: "Blog Posts",
+        folder: "src/pages/posts",
+        create: true,
+        delete: true,
+        fields: [{
+          name: "layout",
+          widget: "hidden",
+          label: "Layout"
+        }, {
+          name: "title",
+          widget: 'string',
+          label: "Title"
+        }, {
+          name: "pubDate",
+          widget: 'datetime',
+          label: "Published date"
+        }, {
+          name: "description",
+          widget: 'string',
+          label: "Description"
+        }, {
+          name: "author",
+          widget: 'string',
+          label: "Author"
+        }, {
+          name: "images",
+          widget: 'list',
+          label: "Images",
+          fields: {
+            name: "image",
+            widget: 'image',
+            label: "Image"
+          }
+        }, {
+          name: "tags",
+          widget: 'list',
+          label: "tags"
+        }]
+      }]
+    }
+  }), preact()]
 });
--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -3,7 +3,7 @@ import Preact, { h} from 'preact';
 import { useState } from 'preact/hooks'; 
 import "../styles/global.css"; 
 
-export default function Greeting({message} : {message: Array<string>})  {
+export default function Greeting({message} : {message: Array<string>}) {
     const [greeting, setGreeting] = useState<string>(message[0]); 
     const randomMessage = () : string => message[(Math.floor(Math.random() * message.length))]; 
     const BeforeStyle = "before:object-fit before:content-[''] before:h-full before:w-0 before:bg-black before:absolute before:inset-0 before:opacity-10 hover:before:w-full before:transform-all before:z-10 before:duration-[500ms]"
@@ -12,9 +12,9 @@ export default function Greeting({message} : {message: Array<string>})  {
         <div>
             <h1>{greeting}</h1>
             <div 
-                class ={`w-fit px-5 py-1 rounded-lg cursor-pointer select-none active:translate-x-[1px] active:translate-y-[1px] relative bg-slate-300 text-white font-bold overflow-hidden ${BeforeStyle} clippedShape`}
+                className ={`w-fit px-5 py-1 rounded-lg cursor-pointer select-none active:translate-x-[1px] active:translate-y-[1px] relative bg-slate-300 text-white font-bold overflow-hidden ${BeforeStyle} clippedShape`}
                 onClick = {()=>setGreeting(randomMessage())}
-            ><div class="z-20 text-white">New Greeting</div></div>
+            >New Greeting</div>
         </div>
     )
 }
